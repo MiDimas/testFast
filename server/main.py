3b3527ea7a2522f27644452e48370e7d72701900
MiDimas <103483149+MiDimas@users.noreply.github.com>
2022-07-29 22:59:25 +0800

    add safe reset

--- server/main.py
+++ server/main.py
@@ -1,17 +1,20 @@
-import json
 from typing import Union
 from fastapi import FastAPI, WebSocket
 
 
 app = FastAPI()
 
+# Объявляется словарь и переменная которая будет служить индексом
 d = {}
 i = 1
 
+# проверка подключения, после подключения отправка данных из словаря, далее в цикле получение json объектов
+# запись в ранее объявленый словарь и отправка словаря на фронт
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    global i
+    global i, d
     await websocket.accept()
+    await websocket.send_json(d)
     while True:
         data = await websocket.receive_json()
         d[i] = data

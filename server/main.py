d9e03480c20be1f80d3731cf9fe8310ff2cbb482
MiDimas <103483149+MiDimas@users.noreply.github.com>
2022-08-01 18:58:45 +0800

    add multiply connect websocket

--- server/main.py
+++ server/main.py
@@ -1,24 +1,45 @@
-from typing import Union
-from fastapi import FastAPI, WebSocket
+from typing import Union, List
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 
 app = FastAPI()
 
-# Объявляется словарь и переменная которая будет служить индексом
-d = {}
-i = 1
+
+# создаем класс управления соединениями и прописываем создание списка активных соединений
+# а так же методы создания соединения и их разрыва, так же метод отправки сообщений отправителю
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_json(message)
+
+
+manager = ConnectionManager()
+
 
 # проверка подключения, после подключения отправка данных из словаря, далее в цикле получение json объектов
 # запись в ранее объявленый словарь и отправка словаря на фронт
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    global i, d
-    await websocket.accept()
-    await websocket.send_json(d)
-    while True:
-        data = await websocket.receive_json()
-        d[i] = data
-        print(d)
-        await websocket.send_json(d)
-        i += 1
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        d = {}
+        i = 1
+        while True:
+            data = await websocket.receive_json()
+            d[i] = data
+            print(d)
+            await manager.send_personal_message(d, websocket)
+            i += 1
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
 
